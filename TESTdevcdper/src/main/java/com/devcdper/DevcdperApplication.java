78c1e14b07656c2f0d2f3d0d33a249044fd089bd
ECS <ECS@DESKTOP-048HU3O>
2021-06-22 16:56:41 +0900

    [03team] TESTdevcdper  teamproject 초기 셋팅 author: 유푸름 file: 초기 셋팅 파일 detail: TESTdevcdper teamproject  git 연습용 TEST파일

---
+++ TESTdevcdper/src/main/java/com/devcdper/DevcdperApplication.java
@@ -0,0 +1,13 @@
+package com.devcdper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcdperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcdperApplication.class, args);
+	}
+
+}
