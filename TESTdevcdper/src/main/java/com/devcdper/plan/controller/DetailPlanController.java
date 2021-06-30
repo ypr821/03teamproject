23a7125bf71f8caa9567c70e73f9a2b505a099b2
RJH <toiydk@gmail.com>
2021-06-30 16:33:13 +0900

    [03team] devcdper v05 teamproject 7차셋팅 - RJH
    author: 류준혁
    
    file:
    -DetailPlanController.java
    
    
    detail:
    -DetailPlanController.java에 등록,수정 메서드 추가

--- TESTdevcdper/src/main/java/com/devcdper/plan/controller/DetailPlanController.java
+++ TESTdevcdper/src/main/java/com/devcdper/plan/controller/DetailPlanController.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -37,14 +38,14 @@ public class DetailPlanController {
 		System.out.println("==============================================");
 		System.out.println("detailPlan 메서드 실행");
 		System.out.println("==============================================");
-		model.addAttribute("title","상사계획 관리 화면");
+		model.addAttribute("title","상세계획 관리 화면");
 		model.addAttribute("function", "plan");			//기능별 left 메뉴노출
 		model.addAttribute("radioCheck", "detailPlan");
 		return "plan/detailPlan/detailPlan";
 	}
 	/*----------------------------------------- 상세 계획 한눈에보기 End----------------------------*/
 	
-	/*----------------------------------------- 학력 상세 계획 관리 Start----------------------------*/
+	/*--------------------------------------::: 학력 상세 계획 관리 Start :::-------------------------*/
 	//학력 상세 계획 관리
 	@GetMapping("/planEducationalHistoryDetail")
 	public String planEducationalHistoryDetail(Model model) {
@@ -59,9 +60,42 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planEducationalHistoryDetail");
 		return "plan/detailPlan/planEducationalHistoryDetail";
 	}
-	/*----------------------------------------- 학력 상세 계획 관리 End----------------------------*/
+		
+	/*----------------------------------------- 학력 상세 계획 관리 등록 Start----------------------------*/
+	@GetMapping("/addPlanEducationalHistoryDetail")
+	public String addPlanEducationalHistoryDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanEducationalHistoryDetail";
+	}
+	@PostMapping("/addPlanEducationalHistoryDetail")
+	public String addPlanEducationalHistoryDetail() {
+		return "redirect:/planEducationalHistoryDetail";
+	}
+	/*----------------------------------------- 학력 상세 계획 관리 등록 End------------------------------*/
+	/*----------------------------------------- 학력 상세 계획 관리 수정 Start----------------------------*/
+	@GetMapping("/modifyPlanEducationalHistoryDetail")
+	public String modifyPlanEducationalHistoryDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanEducationalHistoryDetail";
+	}
 	
-	/*----------------------------------------- 프로젝트 상세 계획 관리 Start----------------------------*/
+	@PostMapping("/modifyPlanEducationalHistoryDetail")
+	public String modifyPlanEducationalHistoryDetail() {
+		return "redirect:/planEducationalHistoryDetail";
+	}
+	/*----------------------------------------- 학력 상세 계획 관리 수정 End------------------------------*/
+	/*----------------------------------------- 학력 상세 계획 관리 삭제 Start----------------------------*/
+	/**삭제 form을 만들어서 할지, modal? popup창을 띄워서  확인 후 삭제 처리 할지 고민중...*/
+	 
+	
+	/*----------------------------------------- 학력 상세 계획 관리 삭제 End------------------------------*/
+	/*--------------------------------------::: 학력 상세 계획 관리 End :::-------------------------*/
+	
+
+	
+	/*--------------------------------------::: 프로젝트 상세 계획 관리 Start :::----------------------------*/
 	//프로젝트 상세 계획 관리
 	@GetMapping("/planProjectDetail")
 	public String planProjectDetail(Model model) {
@@ -76,9 +110,39 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planProjectDetail");
 		return "plan/detailPlan/planProjectDetail";
 	}
-	/*----------------------------------------- 프로젝트 상세 계획 관리 End----------------------------*/
+	/*----------------------------------------- 프로젝트 상세 계획 관리 등록Start -------------------------------*/
+	@GetMapping("/addPlanProjectDetail")
+	public String addPlanProjectDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanProjectDetail";
+	}
+	@PostMapping("/addPlanProjectDetail")
+	public String addPlanProjectDetail() {
+		return "redirect:/planProjectDetail";
+	}
+	/*----------------------------------------- 프로젝트 상세 계획 관리 등록End -------------------------------*/
+	/*----------------------------------------- 프로젝트 상세 계획 관리 수정Start -------------------------------*/
+	@GetMapping("/modifyPlanProjectDetail")
+	public String modifyPlanProjectDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanProjectDetail";
+	}
 	
-	/*----------------------------------------- 자격증 상세 계획 관리 Start----------------------------*/
+	@PostMapping("/modifyPlanProjectDetail")
+	public String modifyPlanProjectDetail() {
+		return "redirect:/planProjectDetail";
+	}
+	/*----------------------------------------- 프로젝트 상세 계획 관리 수정End -------------------------------*/
+	/*----------------------------------------- 프로젝트 상세 계획 관리 삭제Start -------------------------------*/
+	
+	/*----------------------------------------- 프로젝트 상세 계획 관리 삭제End -------------------------------*/
+	/*--------------------------------------::: 프로젝트 상세 계획 관리 End :::-------------------------*/
+	
+	
+	
+	/*--------------------------------------::: 자격증 상세 계획 관리 Start :::-------------------------*/
 	//자격증 상세 계획 관리
 	@GetMapping("/planCertificateDetail")
 	public String planCertificateDetail(Model model) {
@@ -93,9 +157,39 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planCertificateDetail");
 		return "plan/detailPlan/planCertificateDetail";
 	}
-	/*----------------------------------------- 자격증 상세 계획 관리 End----------------------------*/
+	/*----------------------------------------- 자격증 상세 계획 관리 등록 Start ----------------------------*/
+	@GetMapping("/addPlanCertificateDetail")
+	public String addPlanCertificateDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanCertificateDetail";
+	}
+	@PostMapping("/addPlanCertificateDetail")
+	public String addPlanCertificateDetail() {
+		return "redirect:/planCertificateDetail";
+	}
+	/*----------------------------------------- 자격증 상세 계획 관리 등록 End ----------------------------*/
+	/*----------------------------------------- 자격증 상세 계획 관리 수정 Start ----------------------------*/
+	@GetMapping("/modifyPlanCertificateDetail")
+	public String modifyPlanCertificateDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanCertificateDetail";
+	}
 	
-	/*----------------------------------------- 공인어학 상세 계획 관리Start----------------------------*/
+	@PostMapping("/modifyPlanCertificateDetail")
+	public String modifyPlanCertificateDetail() {
+		return "redirect:/planCertificateDetail";
+	}
+	/*----------------------------------------- 자격증 상세 계획 관리 수정 End ----------------------------*/
+	/*----------------------------------------- 자격증 상세 계획 관리 삭제 Start ----------------------------*/
+	
+	/*----------------------------------------- 자격증 상세 계획 관리 삭제 End ----------------------------*/
+	/*--------------------------------------::: 자격증 상세 계획 관리 End :::-------------------------*/
+	
+	
+	
+	/*--------------------------------------::: 공인어학 상세 계획 관리Start :::-------------------------*/
 	//공인어학 상세 계획 관리
 	@GetMapping("/planCertifiedLanguageDetail")
 	public String planCertifiedLanguageDetail(Model model) {
@@ -110,9 +204,39 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planCertifiedLanguageDetail");
 		return "plan/detailPlan/planCertifiedLanguageDetail";
 	}
-	/*----------------------------------------- 공인어학 상세 계획 관리 End----------------------------*/
+	/*----------------------------------------- 공인어학 상세 계획 관리 등록 Start ----------------------------*/
+	@GetMapping("/addPlanCertifiedLanguageDetail")
+	public String addPlanCertifiedLanguageDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanCertifiedLanguageDetail";
+	}
+	@PostMapping("/addPlanCertifiedLanguageDetail")
+	public String addPlanCertifiedLanguageDetail() {
+		return "redirect:/planCertifiedLanguageDetail";
+	}
+	/*----------------------------------------- 공인어학 상세 계획 관리 등록 End ----------------------------*/
+	/*----------------------------------------- 공인어학 상세 계획 관리 수정 Start ----------------------------*/
+	@GetMapping("/modifyPlanCertifiedLanguageDetail")
+	public String modifyPlanCertifiedLanguageDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanCertifiedLanguageDetail";
+	}
 	
-	/*----------------------------------------- 기술스택 상세 계획 관리 Start----------------------------*/
+	@PostMapping("/modifyPlanCertifiedLanguageDetail")
+	public String modifyPlanCertifiedLanguageDetail() {
+		return "redirect:/planCertifiedLanguageDetail";
+	}
+	/*----------------------------------------- 공인어학 상세 계획 관리 수정 End ----------------------------*/
+	/*----------------------------------------- 공인어학 상세 계획 관리 삭제 Start ----------------------------*/
+	
+	/*----------------------------------------- 공인어학 상세 계획 관리 삭제 End ----------------------------*/
+	/*--------------------------------------::: 공인어학 상세 계획 관리 End :::-------------------------*/
+	
+	
+	
+	/*--------------------------------------::: 기술스택 상세 계획 관리 Start :::------------------------*/
 	//기술스택 상세 계획 관리
 	@GetMapping("/planTechnologyStackDetail")
 	public String planTechnologyStackDetail(Model model) {
@@ -127,10 +251,38 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planTechnologyStackDetail");
 		return "plan/detailPlan/planTechnologyStackDetail";
 	}
-	/*----------------------------------------- 기술스택 상세 계획 관리 Start----------------------------*/
+	/*----------------------------------------- 기술스택 상세 계획 관리 등록 Start ---------------------------*/
+	@GetMapping("/addPlanTechnologyStackDetail")
+	public String addPlanTechnologyStackDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanTechnologyStackDetail";
+	}
+	@PostMapping("/addPlanTechnologyStackDetail")
+	public String addPlanTechnologyStackDetail() {
+		return "redirect:/planTechnologyStackDetail";
+	}
+	/*----------------------------------------- 기술스택 상세 계획 관리 등록 End ---------------------------*/
+	/*----------------------------------------- 기술스택 상세 계획 관리 수정 Start ---------------------------*/
+	@GetMapping("/modifyPlanTechnologyStackDetail")
+	public String modifyPlanTechnologyStackDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanTechnologyStackDetail";
+	}
+	
+	@PostMapping("/modifyPlanTechnologyStackDetail")
+	public String modifyPlanTechnologyStackDetail() {
+		return "redirect:/planTechnologyStackDetail";
+	}
+	/*----------------------------------------- 기술스택 상세 계획 관리 수정 End ---------------------------*/
+	/*----------------------------------------- 기술스택 상세 계획 관리 삭제 Start ---------------------------*/
+	
+	/*----------------------------------------- 기술스택 상세 계획 관리 삭제 End ---------------------------*/
+	/*--------------------------------------::: 기술스택 상세 계획 관리 End :::-------------------------*/
 	
 	
-	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 Start----------------------------*/
+	/*--------------------------------------::: 직종전문교육과정 상세 계획 관리 Start :::-------------------------*/
 	//직종전문교육과정 상세 계획 관리
 	@GetMapping("/planJobTrainingDetail")
 	public String planJobTrainingDetail(Model model) {
@@ -145,9 +297,39 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planJobTrainingDetail");
 		return "plan/detailPlan/planJobTrainingDetail";
 	}
-	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 End----------------------------*/
+	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 등록 Start ----------------------------*/
+	@GetMapping("/addPlanJobTrainingDetail")
+	public String addPlanJobTrainingDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanJobTrainingDetail";
+	}
+	@PostMapping("/addPlanJobTrainingDetail")
+	public String addPlanJobTrainingDetail() {
+		return "redirect:/planJobTrainingDetail";
+	}
+	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 등록 End ----------------------------*/
+	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 수정 Start ----------------------------*/
+	@GetMapping("/modifyPlanJobTrainingDetail")
+	public String modifyPlanJobTrainingDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanJobTrainingDetail";
+	}
 	
-	/*----------------------------------------- 인턴십 상세 계획 관리 Start----------------------------*/
+	@PostMapping("/modifyPlanJobTrainingDetail")
+	public String modifyPlanJobTrainingDetail() {
+		return "redirect:/planJobTrainingDetail";
+	}
+	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 수정 End ----------------------------*/
+	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 삭제 Start ----------------------------*/
+	
+	/*----------------------------------------- 직종전문교육과정 상세 계획 관리 삭제 End ----------------------------*/
+	/*--------------------------------------::: 직종전문교육과정 상세 계획 관리 End :::-------------------------*/
+		
+	
+	
+	/*--------------------------------------::: 인턴십 상세 계획 관리 Start :::------------------------*/
 	//인턴십 상세 계획 관리
 	@GetMapping("/planInternshipDetail")
 	public String planInternshipDetail(Model model) {
@@ -162,9 +344,39 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planInternshipDetail");
 		return "plan/detailPlan/planInternshipDetail";
 	}
-	/*----------------------------------------- 인턴십 상세 계획 관리 End----------------------------*/
+	/*----------------------------------------- 인턴십 상세 계획 관리 등록 Start ---------------------------*/
+	@GetMapping("/addPlanInternshipDetail")
+	public String addPlanInternshipDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanInternshipDetail";
+	}
+	@PostMapping("/addPlanInternshipDetail")
+	public String addPlanInternshipDetail() {
+		return "redirect:/planInternshipDetail";
+	}
+	/*----------------------------------------- 인턴십 상세 계획 관리 등록 End ---------------------------*/
+	/*----------------------------------------- 인턴십 상세 계획 관리 수정 Start ---------------------------*/
+	@GetMapping("/modifyPlanInternshipDetail")
+	public String modifyPlanInternshipDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanInternshipDetail";
+	}
 	
-	/*----------------------------------------- 공모전 상세 계획 관리 Start----------------------------*/
+	@PostMapping("/modifyPlanInternshipDetail")
+	public String modifyPlanInternshipDetail() {
+		return "redirect:/planInternshipDetail";
+	}
+	/*----------------------------------------- 인턴십 상세 계획 관리 수정 End ---------------------------*/
+	/*----------------------------------------- 인턴십 상세 계획 관리 삭제 Start ---------------------------*/
+	
+	/*----------------------------------------- 인턴십 상세 계획 관리 삭제 End ---------------------------*/
+	/*--------------------------------------::: 인턴십 상세 계획 관리 End :::-------------------------*/
+	
+	
+	
+	/*--------------------------------------::: 공모전 상세 계획 관리 Start :::------------------------*/
 	//공모전 상세 계획 관리
 	@GetMapping("/planContestDetail")
 	public String planContestDetail(Model model) {
@@ -179,9 +391,39 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planContestDetail");
 		return "plan/detailPlan/planContestDetail";
 	}
-	/*----------------------------------------- 공모전 상세 계획 관리 End----------------------------*/
+	/*----------------------------------------- 공모전 상세 계획 관리 등록 Start ---------------------------*/
+	@GetMapping("/addPlanContestDetail")
+	public String addPlanContestDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanContestDetail";
+	}
+	@PostMapping("/addPlanContestDetail")
+	public String addPlanContestDetail() {
+		return "redirect:/planContestDetail";
+	}
+	/*----------------------------------------- 공모전 상세 계획 관리 등록 End ---------------------------*/
+	/*----------------------------------------- 공모전 상세 계획 관리 수정 Start ---------------------------*/
+	@GetMapping("/modifyPlanContestDetail")
+	public String modifyPlanContestDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanContestDetail";
+	}
 	
-	/*----------------------------------------- 경력 상세 계획 관리 Start----------------------------*/
+	@PostMapping("/modifyPlanContestDetail")
+	public String modifyPlanContestDetail() {
+		return "redirect:/planContestDetail";
+	}
+	/*----------------------------------------- 공모전 상세 계획 관리 수정 End ---------------------------*/
+	/*----------------------------------------- 공모전 상세 계획 관리 삭제 Start ---------------------------*/
+	
+	/*----------------------------------------- 공모전 상세 계획 관리 삭제 End ---------------------------*/
+	/*--------------------------------------::: 공모전 상세 계획 관리 End :::-------------------------*/
+	
+	
+	
+	/*--------------------------------------::: 경력 상세 계획 관리 Start :::-------------------------*/
 	//경력 상세 계획 관리
 	@GetMapping("/planCareerDetail")
 	public String planCareerDetail(Model model) {
@@ -196,7 +438,35 @@ public class DetailPlanController {
 		model.addAttribute("radioCheck", "planCareerDetail");
 		return "plan/detailPlan/planCareerDetail";
 	}
-	/*----------------------------------------- 경력 상세 계획 관리 End----------------------------*/
+	/*----------------------------------------- 경력 상세 계획 관리 등록 Start ----------------------------*/
+	@GetMapping("/addPlanCareerDetail")
+	public String addPlanCareerDetail(Model model) {
+		model.addAttribute("title", "학력 상세계획 등록");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/addPlanCareerDetail";
+	}
+	@PostMapping("/addPlanCareerDetail")
+	public String addPlanCareerDetail() {
+		return "redirect:/planCareerDetail";
+	}
+	/*----------------------------------------- 경력 상세 계획 관리 등록 End ----------------------------*/
+	/*----------------------------------------- 경력 상세 계획 관리 수정 Start ----------------------------*/
+	@GetMapping("/modifyPlanCareerDetail")
+	public String modifyPlanCareerDetail(Model model){
+		model.addAttribute("title", "학력 상세계획 수정");
+		model.addAttribute("function", "none");
+		return "plan/detailPlan/modifyPlanCareerDetail";
+	}
+	
+	@PostMapping("/modifyPlanCareerDetail")
+	public String modifyPlanCareerDetail() {
+		return "redirect:/planCareerDetail";
+	}
+	/*----------------------------------------- 경력 상세 계획 관리 수정 End ----------------------------*/
+	/*----------------------------------------- 경력 상세 계획 관리 삭제 Start ----------------------------*/
+	
+	/*----------------------------------------- 경력 상세 계획 관리 삭제 End ----------------------------*/
+	/*--------------------------------------::: 경력 상세 계획 관리 End :::-------------------------*/
 	
 	
 	
