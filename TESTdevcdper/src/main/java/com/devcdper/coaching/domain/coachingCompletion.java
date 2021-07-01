432e203897c8b5d078c8995172891a80c778836a
YPR <YPR@DESKTOP-4HG6O4O>
2021-07-01 14:06:41 +0900

    [03team] devcdper v06 teamproject 2차셋팅 - YPR author : 유푸름 file : 패키지 : [com.devcdper.coaching.controller] CoachingController.java
    패키지 : [com.devcdper.coaching.domain]
    coachingRFQResult.java
    coachingStop.java
    coachingRFQ.java
    coachingApplyAndPayment.java
    coachingCompletion.java
    
    폴더 coachingModal : resources/templates/coaching/coachingModal/
    modal-coachingStop.html
    modal-Review.html
    modal-RFQ.html
    modal-RFQResult.html
    modal-coaching-totalPlan.html
    modal-coachProfile.html
    
    경로 : [/templates/coaching]
    myCoachingClient.html
    myCoachingCoach.html
    mainCoaching.html
    coachingAdminPage.html
    coachingReview.html
    coachingRFQResult.html
    coachingRFQ.html
    coachAdmin.html
    coachingApplyAndPayment.html
    
    경로 : [/templates]
    main.html
    
    detail :
    com.devcdper.coaching.domain패키지 및 하위 도메인 java파일 추가
    coachingApplyAndPayment.html, modal-coaching-totalPlan.html - 파일 추가
    modal-Review.html ,modal-RFQ.html
    ,modal-RFQResult.html,modal-coaching-totalPlan.html,coachingReview.html
    ,coachingRFQResult.html,coachingRFQ.html,coachingAdminPage.html
    ,myCoachingCoach.html - 카테고리, 검색옵션 수정
    modal-RFQ.html,modal-RFQResult.html,myCoachingClient.html,coachingAdminPage.html
    - 상세보기 모달안에 modal-coaching-totalPlan모달추가
    modal-coachingStop.html - 중단 확인 alert추가
    coachingApplyAndPayment.html - 페이지 내 modal-coachProfile.html 모달 추가
    modal-coachProfile.html - 파일 추가
    mainCoaching.html , coachAdmin.html- 화면 전체 수정
    CoachingController.java - 경로추가

---
+++ TESTdevcdper/src/main/java/com/devcdper/coaching/domain/coachingCompletion.java
@@ -0,0 +1,95 @@
+package com.devcdper.coaching.domain;
+
+public class coachingCompletion {
+
+	private String coachingCompletionCode;
+	private String coachingCategoryCode;
+	private String coachingRFQResultCode;
+	private String coachingProgressCode;
+	private String totalPlanCode;	
+	private String planDetailCode;	
+	private String paymentCode;
+	private String coachUserEmail;
+	private String userEmail;
+	private String coachingCompletionFinalResult;	
+	private String coachingCompletionRegisterDate;
+	
+	public String getCoachingCompletionCode() {
+		return coachingCompletionCode;
+	}
+	public void setCoachingCompletionCode(String coachingCompletionCode) {
+		this.coachingCompletionCode = coachingCompletionCode;
+	}
+	public String getCoachingCategoryCode() {
+		return coachingCategoryCode;
+	}
+	public void setCoachingCategoryCode(String coachingCategoryCode) {
+		this.coachingCategoryCode = coachingCategoryCode;
+	}
+	public String getCoachingRFQResultCode() {
+		return coachingRFQResultCode;
+	}
+	public void setCoachingRFQResultCode(String coachingRFQResultCode) {
+		this.coachingRFQResultCode = coachingRFQResultCode;
+	}
+	public String getCoachingProgressCode() {
+		return coachingProgressCode;
+	}
+	public void setCoachingProgressCode(String coachingProgressCode) {
+		this.coachingProgressCode = coachingProgressCode;
+	}
+	public String getTotalPlanCode() {
+		return totalPlanCode;
+	}
+	public void setTotalPlanCode(String totalPlanCode) {
+		this.totalPlanCode = totalPlanCode;
+	}
+	public String getPlanDetailCode() {
+		return planDetailCode;
+	}
+	public void setPlanDetailCode(String planDetailCode) {
+		this.planDetailCode = planDetailCode;
+	}
+	public String getPaymentCode() {
+		return paymentCode;
+	}
+	public void setPaymentCode(String paymentCode) {
+		this.paymentCode = paymentCode;
+	}
+	public String getCoachUserEmail() {
+		return coachUserEmail;
+	}
+	public void setCoachUserEmail(String coachUserEmail) {
+		this.coachUserEmail = coachUserEmail;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getCoachingCompletionFinalResult() {
+		return coachingCompletionFinalResult;
+	}
+	public void setCoachingCompletionFinalResult(String coachingCompletionFinalResult) {
+		this.coachingCompletionFinalResult = coachingCompletionFinalResult;
+	}
+	public String getCoachingCompletionRegisterDate() {
+		return coachingCompletionRegisterDate;
+	}
+	public void setCoachingCompletionRegisterDate(String coachingCompletionRegisterDate) {
+		this.coachingCompletionRegisterDate = coachingCompletionRegisterDate;
+	}
+	@Override
+	public String toString() {
+		return "coachingCompletion [coachingCompletionCode=" + coachingCompletionCode + ", coachingCategoryCode="
+				+ coachingCategoryCode + ", coachingRFQResultCode=" + coachingRFQResultCode + ", coachingProgressCode="
+				+ coachingProgressCode + ", totalPlanCode=" + totalPlanCode + ", planDetailCode=" + planDetailCode
+				+ ", paymentCode=" + paymentCode + ", coachUserEmail=" + coachUserEmail + ", userEmail=" + userEmail
+				+ ", coachingCompletionFinalResult=" + coachingCompletionFinalResult
+				+ ", coachingCompletionRegisterDate=" + coachingCompletionRegisterDate + "]";
+	}
+	
+	
+	
+}
---
+++ TESTdevcdper/src/main/java/com/devcdper/coaching/domain/coachingStop.java
@@ -0,0 +1,110 @@
+package com.devcdper.coaching.domain;
+
+public class coachingStop {
+
+	private String coachingStopCode;
+	private String coachingCategoryCode;
+	private String coachingRFQResultCode;
+	private String coachingProgressCode;
+	private String totalPlanCode;
+	private String planDetailCode;	
+	private String paymentCode;
+	private String coachUserEmail;	
+	private String userEmail;
+	private String coachingStopReason;
+	private String coachingRemainingPeriod;	
+	private String coachingRefundAmount;	
+	private String coachingStopDate;
+	
+	
+	public String getCoachingStopCode() {
+		return coachingStopCode;
+	}
+	public void setCoachingStopCode(String coachingStopCode) {
+		this.coachingStopCode = coachingStopCode;
+	}
+	public String getCoachingCategoryCode() {
+		return coachingCategoryCode;
+	}
+	public void setCoachingCategoryCode(String coachingCategoryCode) {
+		this.coachingCategoryCode = coachingCategoryCode;
+	}
+	public String getCoachingRFQResultCode() {
+		return coachingRFQResultCode;
+	}
+	public void setCoachingRFQResultCode(String coachingRFQResultCode) {
+		this.coachingRFQResultCode = coachingRFQResultCode;
+	}
+	public String getCoachingProgressCode() {
+		return coachingProgressCode;
+	}
+	public void setCoachingProgressCode(String coachingProgressCode) {
+		this.coachingProgressCode = coachingProgressCode;
+	}
+	public String getTotalPlanCode() {
+		return totalPlanCode;
+	}
+	public void setTotalPlanCode(String totalPlanCode) {
+		this.totalPlanCode = totalPlanCode;
+	}
+	public String getPlanDetailCode() {
+		return planDetailCode;
+	}
+	public void setPlanDetailCode(String planDetailCode) {
+		this.planDetailCode = planDetailCode;
+	}
+	public String getPaymentCode() {
+		return paymentCode;
+	}
+	public void setPaymentCode(String paymentCode) {
+		this.paymentCode = paymentCode;
+	}
+	public String getCoachUserEmail() {
+		return coachUserEmail;
+	}
+	public void setCoachUserEmail(String coachUserEmail) {
+		this.coachUserEmail = coachUserEmail;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getCoachingStopReason() {
+		return coachingStopReason;
+	}
+	public void setCoachingStopReason(String coachingStopReason) {
+		this.coachingStopReason = coachingStopReason;
+	}
+	public String getCoachingRemainingPeriod() {
+		return coachingRemainingPeriod;
+	}
+	public void setCoachingRemainingPeriod(String coachingRemainingPeriod) {
+		this.coachingRemainingPeriod = coachingRemainingPeriod;
+	}
+	public String getCoachingRefundAmount() {
+		return coachingRefundAmount;
+	}
+	public void setCoachingRefundAmount(String coachingRefundAmount) {
+		this.coachingRefundAmount = coachingRefundAmount;
+	}
+	public String getCoachingStopDate() {
+		return coachingStopDate;
+	}
+	public void setCoachingStopDate(String coachingStopDate) {
+		this.coachingStopDate = coachingStopDate;
+	}
+	@Override
+	public String toString() {
+		return "coachingStop [coachingStopCode=" + coachingStopCode + ", coachingCategoryCode=" + coachingCategoryCode
+				+ ", coachingRFQResultCode=" + coachingRFQResultCode + ", coachingProgressCode=" + coachingProgressCode
+				+ ", totalPlanCode=" + totalPlanCode + ", planDetailCode=" + planDetailCode + ", paymentCode="
+				+ paymentCode + ", coachUserEmail=" + coachUserEmail + ", userEmail=" + userEmail
+				+ ", coachingStopReason=" + coachingStopReason + ", coachingRemainingPeriod=" + coachingRemainingPeriod
+				+ ", coachingRefundAmount=" + coachingRefundAmount + ", coachingStopDate=" + coachingStopDate + "]";
+	}
+	
+	
+	
+}
