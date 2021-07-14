8af1037d6e6c806401867df8bbb5a026fe87c1ba
Woolly <wool_ly@naver.com>
2021-07-14 23:01:57 +0900

    [03team] devcdper v08 teamproject 1차셋팅 - YSG
    author : 양슬기
    -------------------------------------------------------
    [src/main/java]
    [com.devcdper.challenge.controller]
    AdminChallengeController.java
    ChallengeController.java
    -------------------------------------------------------
    [com.devcdper.challenge.dao]
    ChallengeMapper.java
    -------------------------------------------------------
    [com.devcdper.challenge.domain]
    Challenge.java
    ChallengeAchievementRate.java
    ChallengeCategory.java
    ChallengeCertification.java
    ChallengeParticipation.java
    -------------------------------------------------------
    [com.devcdper.challenge.service]
    ChallengeService.java
    ----------------------------------------------
    [src/main/resources]
    ChallengeMapper.xml
    -------------------------------------------------------
    [src/main/resources/templates/challenge/admin]
    adminChallenge.html
    adminChallengeAchievement.html
    adminChallengeCompensation.html
    adminChallengeParticipation.html
    adminChallengeRanking.html
    adminChallengeReport.html
    -------------------------------------------------------
    [src/main/resources/templates/challenge/user]
    challengeCertification.html
    challengeCertificationBoard.html
    challengeCertificationBoardView.html
    challengeCertificationInsert.html
    challengeExploration.html
    challengeExplorationDetailInfo.html
    challengeInsert.html
    challengeParticipation.html
    myChallenge.html
    myChallengeCompleteList.html
    myChallengeDetailInfo.html
    myChallengeEdit.html
    myChallengeInsertList.html
    myChallengeParticipationList.html
    mainChallenge.html
    challengeAdminDefault.html
    challengeUserDefault.html
    challengeModalAchievementDetail.html
    challengeModalCertificationDetail.html
    challengeModalCompensation.html
    challengeModalInsertDetail.html
    challengeModalParticipationDetail.html
    challengeModalReportDetail.html
    challengeModalCertificationMethodsModify.html
    challengeModalDateModify.html
    challengeModalIntroduceModify.html
    challengeModalMainImageModify.html
    challengeModalNameModify.html
    challengeModalTagModify.html
    challengeUserLeft.html
    
    myChallengeSearchForm.html
    -------------------------------------------------------
    [src/main/resources/static/AdminLTE3/dist/img]
    
    fivemedal.png
    fourthmedal.png
    goldmedal.png
    silvermedal.png
    bronzemedal.png
    
    detail
    -------------------------- 비기능 관련 --------------------------
    1) AdminMapperChallenge.java -> 삭제
    2) 두배러 챌린지 메인 화면 이미지 리소스
    추가(fivemedal.png/fourthmedal.png/goldmedal.png/silvermedal.png/bronzemedal.png)
    3) 나의 챌린지 - myChallengeCompleteList.html / myChallengeInsertList.html /
    myChallengeParticipationList.html
    검색폼 fragments 추가 - myChallengeSearchForm.html
    
    -------------------------- 기능 관련 --------------------------
    
    1) 나의 챌린지[myChallenge] - 개설 챌린지[myChallengeInsertList] - 챌린지
    설정[myChallengeEdit]
    [완료] 각각의 변경 버튼 클릭시 Modal창에 값 전달
    [완료] 챌린지명 / 챌린지 기간 / 챌린지 인증방법 / 챌린지 소개 / 챌린지 태그 1차적 업데이트 기능 구현
    
    2) 챌린지 개설하기 challengeInsert.html
    챌린지 인증하기 challengeCertification.html
    나의 챌린지 myChallenge.html
    
    ChallengeController.java
    [완료] 세션 아이디를 통한 접근 권한 설정 및 로그인 화면으로 리턴

--- TESTdevcdper/src/main/java/com/devcdper/challenge/controller/AdminChallengeController.java
+++ TESTdevcdper/src/main/java/com/devcdper/challenge/controller/AdminChallengeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.devcdper.challenge.domain.Challenge;
 import com.devcdper.challenge.service.ChallengeService;
@@ -27,7 +28,7 @@ public class AdminChallengeController {
 	}
 	
 	
-
+	/*-------------------------------------AdminChallengeControllerInit 메서드 시작----------------------------------------*/
 	@PostConstruct
 	public void AdminChallengeControllerInit() {
 
@@ -43,7 +44,10 @@ public class AdminChallengeController {
 		 System.out.println("========================================");
 		 
 	}
-
+	/*-------------------------------------AdminChallengeControllerInit 메서드 끝----------------------------------------*/
+	
+	
+	/*------------------------------------------------챌린지 가이드 시작-----------------------------------------------------*/
 	@GetMapping("/mainChallenge")
 	public String mainChallenge(Model model) {
 		
@@ -54,22 +58,42 @@ public class AdminChallengeController {
 		model.addAttribute("challengeExplorationList", challengeExplorationList);
 		return "challenge/mainChallenge";
 	}
+	/*------------------------------------------------챌린지 가이드 끝-----------------------------------------------------*/
+	
+	
 
+	/*------------------------------------------------챌린지 관리 시작-----------------------------------------------------*/
 	@GetMapping("/adminChallenge")
-	public String adminChallenge(Model model, @RequestParam(value="currentPage", required = false, defaultValue = "1") int currentPage) {
+	public String adminChallenge(Model model, @RequestParam(value="currentPage", required = false, defaultValue = "1") int currentPage, String challengeName) {
 		
-		Map<String, Object> resultMap = challengeService.getChallengeList(currentPage);
+		List<Challenge> challengeList = challengeService.getChallengeList(currentPage, challengeName);
+		System.out.println("challengeList : "+ challengeList);
 		
 		model.addAttribute("title", "챌린지 개설 관리");
-		model.addAttribute("currentPage", 					currentPage);
-		model.addAttribute("lastPage", 						resultMap.get("lastPage"));
-		model.addAttribute("pageStartNum", 					resultMap.get("pageStartNum"));
-		model.addAttribute("pageEndNum", 					resultMap.get("pageEndNum"));
-		model.addAttribute("challengeList", 				resultMap.get("challengeList"));
+		/*
+			model.addAttribute("currentPage", 					currentPage);
+		 * model.addAttribute("lastPage", resultMap.get("lastPage"));
+		 * model.addAttribute("pageStartNum", resultMap.get("pageStartNum"));
+		 * model.addAttribute("pageEndNum", resultMap.get("pageEndNum"));
+		 */
+		model.addAttribute("challengeList", challengeList);
 		model.addAttribute("radioCheck", "adminChallenge");
 		return "challenge/admin/adminChallenge";
 	}
 
+	@GetMapping("/challengeModalInsertDetail")
+	public String challengeModalInsertDetail(Model model) {
+		
+		model.addAttribute("title", "챌린지 모달");
+		model.addAttribute("radioCheck", "challengeModalInsertDetail");
+		return "challengeModal/adminChallengeModal/challengeModalInsertDetail";
+	}
+	/*------------------------------------------------챌린지 관리 끝-----------------------------------------------------*/
+	
+	
+	
+	
+	/*------------------------------------------------챌린지 참여 관리 시작-----------------------------------------------------*/
 	@GetMapping("/adminChallengeParticipation")
 	public String adminChallengeParticipation(Model model, @RequestParam(value="currentPage", required = false, defaultValue = "1") int currentPage) {
 		
@@ -84,14 +108,23 @@ public class AdminChallengeController {
 		model.addAttribute("radioCheck", "adminChallengeParticipation");
 		return "challenge/admin/adminChallengeParticipation";
 	}
-
+	/*------------------------------------------------챌린지 참여 관리 끝-----------------------------------------------------*/
+	
+	
+	
+	
+	/*------------------------------------------------챌린지 신고 관리 시작-----------------------------------------------------*/
 	@GetMapping("/adminChallengeReport")
 	public String adminChallengeReport(Model model) {
 		model.addAttribute("title", "챌린지 신고 관리");
 		model.addAttribute("radioCheck", "adminChallengeReport");
 		return "challenge/admin/adminChallengeReport";
 	}
+	/*------------------------------------------------챌린지 신고 관리 끝-----------------------------------------------------*/
 
+	
+	
+	/*------------------------------------------------챌린지 달성율 관리 시작-----------------------------------------------------*/
 	@GetMapping("/adminChallengeAchievement")
 	public String adminChallengeAchievement(Model model, @RequestParam(value="currentPage", required = false, defaultValue = "1") int currentPage) {
 		
@@ -106,19 +139,25 @@ public class AdminChallengeController {
 		model.addAttribute("radioCheck", "adminChallengeAchievement");
 		return "challenge/admin/adminChallengeAchievement";
 	}
-
+	/*------------------------------------------------챌린지 달성율 관리 끝-----------------------------------------------------*/
+	
+	
+	/*------------------------------------------------챌린지 최종순위 리스트 시작-----------------------------------------------------*/
 	@GetMapping("/adminChallengeRanking")
 	public String adminChallengeRanking(Model model) {
 		model.addAttribute("title", "챌린지 최종순위 관리");
 		model.addAttribute("radioCheck", "adminChallengeRanking");
 		return "challenge/admin/adminChallengeRanking";
 	}
+	/*------------------------------------------------챌린지 최종순위 리스트 끝-----------------------------------------------------*/
 
+	
+	/*------------------------------------------------챌린지 보상지급 관리 시작-----------------------------------------------------*/
 	@GetMapping("/adminChallengeCompensation")
 	public String challengeCompensation(Model model) {
 		model.addAttribute("title", "챌린지 보상지급 관리");
 		model.addAttribute("radioCheck", "adminChallengeCompensation");
 		return "challenge/admin/adminChallengeCompensation";
 	}
-
+	/*------------------------------------------------챌린지 보상지급 관리 끝-----------------------------------------------------*/
 }
