fc60f9edecc47b7aa705bedf612c74634d1733b7
psm <psm@218.38.137.28>
2021-07-26 12:38:11 +0900

    [03team] devcdper v10 teamproject 1차셋팅 - PSM
    auther : 박승민
    message : "이번에 기능구현을 별로 못했네요..게을러진거같아요. 정신차리고 분발하갰습니다!"
    
    <!-- src/main/java -->
    [com.devcdper.coaching.domain]
    file : CoachingApplyAndPayment.java
    detail : 클래스명이 소문자로 인식되어 기존파일 삭제후 정상적인 파일명으로
    		새로운 클래스파일 생성 후 기존 코드 전체 복사붙여넣기 하였습니다.
    
    [com.devcdper.plan.controller]
    file : PlanController.java
    detail : 나의 계획 한눈에 보기 통합계획 선택에 따른 하위 계획 전체 노출  AJAX생성
    		 나의 계획 한눈에 보기 계획카테고리 선택에 따른 계획 노출 AJAX생성
    
    [com.devcdper.plan.service]
    file : PlanService.java
    detail : 한명 회원에 대한 각 계획 항목별 조회를 위한 메서드 선언
    		(학력,프로젝트,자격증,공인어학,기술스택,직종전문교육과정,인턴십,공모전,경력)
    
    [com.devcdper.plan.dao]
    file : PlanMapper.java
    detail : 한명 회원에 대한 각 계획 항목별 조회를 위한 추상메서드 선언
    		(학력,프로젝트,자격증,공인어학,기술스택,직종전문교육과정,인턴십,공모전,경력)
    
    <!-- src/main/resources -->
    [mapper]
    file : PlanMapper.xml
    detail : 한명 회원에 대한 각 계획 항목별 조회를 위한 쿼리 생성
    		(학력,프로젝트,자격증,공인어학,기술스택,직종전문교육과정,인턴십,공모전,경력)
    
    [templates / plan]
    file : mainPlan.html
    detail : 나의 계획 한눈에 보기에서 통합계획 선택에 따른 하위 계획 전체 AJAX통신하여
    		 노출기능 및 카테고리 선택시 해당 카테고리 계획 AJAX통신하여 노출기능 구현

--- TESTdevcdper/src/main/java/com/devcdper/plan/dao/PlanMapper.java
+++ TESTdevcdper/src/main/java/com/devcdper/plan/dao/PlanMapper.java
@@ -12,7 +12,34 @@ public interface PlanMapper {
 	// 통합계획 등록
 	public int addTotalPlan(PlanDto planDto);
 	
-	// 한명회원 통합계획 전체조회
+	// 한명회원 통합계획 조회
 	public List<PlanDto> getTotalPlan(String searchKey, String searchValue);
+	
+	// 한명회원 학력계획 조회
+	public List<PlanDto> getEducationalHistoryPlan(String searchKey, String searchValue);
+	
+	// 한명회원 프로젝트계획 조회
+	public List<PlanDto> getProjectPlan(String searchKey, String searchValue);
+	
+	// 한명회원 자격증 조회
+	public List<PlanDto> getCertificatePlan(String searchKey, String searchValue);
+	
+	// 한명회원 공인어학 조회
+	public List<PlanDto> getCertifiedLanguagePlan(String searchKey, String searchValue);
+	
+	// 한명회원 기술스택 조회
+	public List<PlanDto> getTechnologyStackPlan(String searchKey, String searchValue);
+	
+	// 한명회원 직종전문교육과정 조회
+	public List<PlanDto> getJobTrainingPlan(String searchKey, String searchValue);
+	
+	// 한명회원 인턴십 조회
+	public List<PlanDto> getInternshipPlan(String searchKey, String searchValue);
+	
+	// 한명회원 공모전 조회
+	public List<PlanDto> getContestPlan(String searchKey, String searchValue);
+	
+	// 한명회원 경력 조회
+	public List<PlanDto> getCareerPlan(String searchKey, String searchValue);
 	
 }
--- TESTdevcdper/src/main/java/com/devcdper/plan/service/PlanService.java
+++ TESTdevcdper/src/main/java/com/devcdper/plan/service/PlanService.java
@@ -30,7 +30,7 @@ public class PlanService {
 		return result;
 	};
 	
-	// 한명회원 통합계획 전체조회
+	// 한명회원 통합계획 조회
 	public List<PlanDto> getTotalPlan(String searchKey, String searchValue){
 		List<PlanDto> planDto = null;
 		if(searchKey != null && !"".equals(searchKey)) {
@@ -38,11 +38,134 @@ public class PlanService {
 			if(searchValue != null && !"".equals(searchValue)) {
 				System.out.println(searchValue + " <<- searchValue : PlanService / getTotalPlan()");
 				planDto = planMapper.getTotalPlan(searchKey, searchValue);
-			}
-		}
+			};
+		};
 		return planDto;
 	};
 	
+	// 한명회원 학력계획 조회
+	public List<PlanDto> getEducationalHistoryPlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getEducationalHistoryPlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getEducationalHistoryPlan()");
+				planDto = planMapper.getEducationalHistoryPlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
 	
+	// 한명회원 프로젝트계획 조회
+	public List<PlanDto> getProjectPlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getProjectPlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getProjectPlan()");
+				planDto = planMapper.getProjectPlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
 	
+	// 한명회원 자격증 조회
+	public List<PlanDto> getCertificatePlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getCertificatePlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getCertificatePlan()");
+				planDto = planMapper.getCertificatePlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
+	
+	// 한명회원 공인어학 조회
+	public List<PlanDto> getCertifiedLanguagePlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getCertifiedLanguagePlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getCertifiedLanguagePlan()");
+				planDto = planMapper.getCertifiedLanguagePlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
+	
+	// 한명회원 기술스택 조회
+	public List<PlanDto> getTechnologyStackPlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getTechnologyStackPlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getTechnologyStackPlan()");
+				planDto = planMapper.getTechnologyStackPlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
+	
+	// 한명회원 직종전문교육과정 조회
+	public List<PlanDto> getJobTrainingPlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getJobTrainingPlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getJobTrainingPlan()");
+				planDto = planMapper.getJobTrainingPlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
+	
+	// 한명회원 인턴십 조회
+	public List<PlanDto> getInternshipPlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getInternshipPlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getInternshipPlan()");
+				planDto = planMapper.getInternshipPlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
+	
+	// 한명회원 공모전 조회
+	public List<PlanDto> getContestPlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getContestPlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getContestPlan()");
+				planDto = planMapper.getContestPlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
+	
+	// 한명회원 경력 조회
+	public List<PlanDto> getCareerPlan(String searchKey, String searchValue){
+		List<PlanDto> planDto = null;
+		if(searchKey != null && !"".equals(searchKey)) {
+			System.out.println(searchKey + " <<- searchKey : PlanService / getCareerPlan()");
+			if(searchValue != null && !"".equals(searchValue)) {
+				System.out.println(searchValue + " <<- searchValue : PlanService / getCareerPlan()");
+				planDto = planMapper.getCareerPlan(searchKey, searchValue);
+				return planDto;
+			};
+		};
+		return planDto;
+	};
 };
